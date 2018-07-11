3bb7015f762f5b24f6413d2c44b73f4dc631a73d
Anton Kosykh <kelin2025@yandex.ru>
2018-07-11 14:18:05 +0300

    cleanup-files.js

---
+++ cleanup-files.js
@@ -0,0 +1,18 @@
+function isFileIgnored(file) {
+  for (var i in ignoredFiles) {
+    if (typeof ignoredFiles[i] === "string" && file === ignoredFiles[i]) {
+      return true
+    }
+
+    if (ignoredFiles[i] instanceof RegExp && ignoredFiles[i].test(file)) {
+      return true
+    }
+  }
+  return false
+}
+
+module.exports = function cleanupFiles(files) {
+  return files.filter(function(file) {
+    return !isFileIgnored(file) && file !== "package.json"
+  })
+}
