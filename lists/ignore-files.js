2c7ba59c197698e29237fba5182736a235413b97
Anton Kosykh <kelin2025@yandex.ru>
2018-07-11 14:18:24 +0300

    Ignore lists for files and package.json fields

---
+++ lists/ignore-files.js
@@ -0,0 +1,17 @@
+module.exports = [
+  ".git",
+  ".vscode",
+  "yarn.lock",
+  /^\.eslintrc/,
+  ".babelrc",
+  ".jsdocrc",
+  "appveyor.yml",
+  "node_modules",
+  ".size-limit",
+  ".travis.yml",
+  ".yaspellerrc",
+  ".editorconfig",
+  ".lintstagedrc",
+  "jest.config.js",
+  "yarn-error.log"
+]
---
+++ lists/package-json-fields.js
@@ -0,0 +1,17 @@
+module.exports = [
+  "name",
+  "version",
+  "description",
+  "keywords",
+  "main",
+  "module",
+  "browser",
+  "repository",
+  "bugs",
+  "homepage",
+  "author",
+  "license",
+  "dependencies",
+  "peerDependencies",
+  "sideEffects"
+]
